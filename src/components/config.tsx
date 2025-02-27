735bbf747f9c2315283c70f8c31745af21bdad05
Sebwin47 <sebastian.windsperger@gmx.at>
2025-02-27 23:47:10 +0100

    fix: update API_BASE_URL to use environment variable and add .env to .gitignore

--- src/components/config.tsx
+++ src/components/config.tsx
@@ -1,3 +1,2 @@
-const API_BASE_URL = "https://vms-server-bn6m.onrender.com";
-//const API_BASE_URL = "http://localhost:3322";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default API_BASE_URL;
